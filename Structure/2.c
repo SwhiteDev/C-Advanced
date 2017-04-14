713d58fb6a6009ef009aa65ae85f28b1ecd25f9c
Swhite <1779011982@qq.com>
2017-04-14 20:47:12 +0800

    源文件

--- Structure/2.c
+++ Structure/2.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 #define TRUE 0
 #define FALSE -1
 
+static int a = 10;
 
-static char a = 10;
-
+void func(int *arg)
+{
+	*arg += 1; 
+}
 
 void main()
 {
+	int b = 11;
+	int *p = malloc(sizeof(b));
+
+	func(&b);
+	func(&a);
+
+	printf("局部变量b的值是：%d\n",b);
 	printf("静态变量a的值是：%d\n",a);
 }
